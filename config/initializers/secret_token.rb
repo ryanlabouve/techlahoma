533b97ea40b687f42628bf5beeff3a949347b154
Jeremy Green <jeremy@octolabs.com>
2013-12-26 21:46:07 -0600

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Techlahoma::Application.config.secret_key_base = '992a434aa7295b0e0ea763abb8ff0e670c4a4758bed82d9e57d7aac17aa549d21b8d97bb991d1a723d2b6601bdaf70f8c876ec137d1532956028cbedace94ce6'
