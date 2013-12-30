f242ec2b30b441efea370c7bec3062dc9ace205b
Jeremy Green <jeremy@octolabs.com>
2013-12-29 23:07:21 -0600

    Some bug fixes on the initial OmniAuth integration.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < ApplicationController
+  protect_from_forgery :except => :create
   def new
   end
 
   def create
+    Rails.logger.debug "omniauth.auth --------------"
+    Rails.logger.debug env["omniauth.auth"]
     authentication = Authentication.from_omniauth(env["omniauth.auth"])
     user = authentication.user || authentication.create_user
     session[:user_id] = user.id
@@ -13,5 +16,4 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, notice: "Signed out!"
   end
-
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
   provider :github, ENV['GITHUB_CONSUMER_KEY'], ENV['GITHUB_CONSUMER_SECRET']
 end
