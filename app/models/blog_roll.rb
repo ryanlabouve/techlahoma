557ae1d9032782b201ed5111cc9a1b00aa813d61
rob sullivan <rob@datachomp.com>
2014-05-31 10:08:29 -0500

    use google instead of the old feed parser. thanks jeremy

--- app/models/blog_roll.rb
+++ app/models/blog_roll.rb
@@ -1,4 +1,4 @@
-require 'feed-normalizer'
+require "googleajax"
 class BlogRoll < ActiveRecord::Base
   self.table_name = 'public.blog_rolls'
   has_many :blog_entries
@@ -18,61 +18,44 @@ class BlogRoll < ActiveRecord::Base
       roll.feed_uri = feed_uri
       roll.feed_type = feed_type
       roll.save
-      
+
       #back fill the last 3 months
       parse_feed_normal(roll.feed_uri, roll.id, Time.now - 3.months)
   end
 
 
-
   def self.parse_feed_normal(feed_uri, blog_id, datefilter)
-    
-    # this method is still pretty gross and needs help
-    counter = 2
+    GoogleAjax.referrer = "techlahoma.com"
+
     begin
-        p counter
-        feed = FeedNormalizer::FeedNormalizer.parse open(feed_uri)
-        #feed = FeedNormalizer::FeedNormalizer.parse open("http://datachomp.com/atom.xml")
-        if feed.entries.nil?
-          sleep 2
-          p 'the lion sleeps tonight'
-          feed = FeedNormalizer::FeedNormalizer.parse open(feed_uri)
-        end
-      rescue Timeout::Error => e
-        p 'timeouts'  
-        p e.message
-        return false
-      rescue OpenURI::HTTPError => e
-        p 'this whole thing is broken'
-        p e.message
-        return false
-      rescue NoMethodError => e
-        p 'no method here'
-        p e.message
-        return false
-      rescue RuntimeError  => e
-        p 'it broke'
+        feedinfo = GoogleAjax::Feed.load(feed_uri)
+        feedinfo[:entries].each do |c|
 
-        p e.message
-        (counter-=1) < 0 ? raise : retry  #lets just rety twice
-      end
-        
-    feed.entries.each do |item|
-      dateland = item.date_published  || item.last_updated
+          if c[:published_date].to_date > datefilter
+            toppy = BlogEntry.where("entry_uri = ?", c[:link]).select(:id).first
 
-      if dateland.to_date > datefilter
-        
-        toppy = BlogEntry.where("entry_uri = ?", item.id).select(:id).first
-        if toppy.nil?
-          entry = BlogEntry.new
-          entry.blog_roll_id = blog_id
-          entry.title = item.title
-          entry.entry_uri = item.id
-          entry.posted_date = dateland
-          entry.save
-        end
-      end  #end if
+            if toppy.nil?
+              entry = BlogEntry.new
+              entry.blog_roll_id = blog_id
+              entry.title = c[:title]
+              entry.entry_uri = c[:link]
+              entry.posted_date = c[:published_date]
+              entry.save
+            end
+          end#end date check
+        end #end feed info loop
     end
   end
 
 end
+=begin
+BlogRoll.create('Jordan Rousseau', 'Jordan Rousseau', 'http://rousseau.io/', 'http://rousseau.io/atom.xml', 'atom' )
+BlogRoll.create('Groove Coder', 'Luke Crouch', 'http://groovecoder.com/', 'http://groovecoder.com/feed/', 'feed' )
+Luke Crouch - http://groovecoder.com/feed/
+BlogRoll.create('Buddy Lindsey', 'Buddy Lindsey', 'http://buddylindsey.com/', 'http://buddylindsey.com/feed/', 'live' )
+
+
+BlogRoll.create('Devin Clark', 'Devin Clark', 'http://devin-clark.com/', 'http://devin-clark.com/rss/', 'rss' )
+BlogRoll.create('Jesse Harlin', 'Jesse Harlin', 'http://www.simiansblog.com/', 'http://www.simiansblog.com/rss?containerid=52', 'word' )
+http://devin-clark.com/rss/
+=end
